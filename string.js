e6de7c3c86944d44bf759e2e8c1b1b84902c1bb3
saiful797 <saifulislamsaif797@gmail.com>
2024-02-24 11:32:40 +0600

    Added string.js file

---
+++ string.js
@@ -0,0 +1,8 @@
+
+const firstSentence = "I am a web developer.";
+const secondSentence = "I love to Code.";
+const thirdSentence = "I love to eat biriyani.";
+
+const paragraph = `${firstSentence} ${secondSentence} ${thirdSentence}`;
+
+console.log(paragraph);
